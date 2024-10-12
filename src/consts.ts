e67f1afb57b77dc4189c0b3d40e845d816202ad2
Paul <john.paul.abrams@gmail.com>
2024-10-12 01:39:37 -0400

    site title and desc

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Field of Azure';
+export const SITE_DESCRIPTION = 'A technical resource for users of Azure Cloud services such as Azure DevOps';
