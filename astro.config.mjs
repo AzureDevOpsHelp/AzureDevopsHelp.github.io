95cef7eba61e86c68c153899ab9a9328cb779b7e
Paul Abrams <john.paul.abrams@gmail.com>
2025-03-22 16:42:43 -0400

    astro config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
 	base: "/AzureDevOpsHelp.github.io/",
+	site: "https://azuredevopshelp.github.io",
 	output: "static",
 	integrations: [mdx(), sitemap()],
 });
